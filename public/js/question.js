01b5e72b75fa10cb853dbe1d2ff3461b6fa837aa
chinedu ofor <oforchinedu12@gmail.com>
2017-01-19 16:24:16 +0100

    final Project

--- public/js/question.js
+++ public/js/question.js
@@ -29,6 +29,85 @@ $(document).ready(function(){
 	var username = localStorage.getItem("username")
 	var subject = localStorage.getItem("choice").toLowerCase();
 
+
+	//Clock pasing time
+	var deadline = new Date(Date.parse(new Date()) +  60 * 1000);
+	  //var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+
+	//initialize clock
+  	function clockInit(){
+	    initializeClock('clockdiv', deadline);
+	}
+
+
+
+	//Start Timer function
+	function getTimeRemaining(endtime) {
+	    var t = Date.parse(endtime) - Date.parse(new Date());
+	    var seconds = Math.floor((t / 1000) % 60);
+	    var minutes = Math.floor((t / 1000 / 60) % 60);
+	    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	    return {
+	      'total': t,
+	      'days': days,
+	      'hours': hours,
+	      'minutes': minutes,
+	      'seconds': seconds
+	    };
+	}
+
+
+
+	//function to initialize Clock
+	function initializeClock(id, endtime) {
+	  	var clock = document.getElementById(id);
+	  	var daysSpan = clock.querySelector('.days');
+	  	var hoursSpan = clock.querySelector('.hours');
+	  	var minutesSpan = clock.querySelector('.minutes');
+	  	secondsSpan = clock.querySelector('.seconds');
+
+	  	function updateClock() {
+	      var t = getTimeRemaining(endtime);
+
+	      daysSpan.innerHTML = t.days;
+	      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+	      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+	      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+	      if (t.total <= 0) {
+	        getSelected()
+	        end.play();
+	        clearInterval(timeinterval);
+	        
+	        calcScore();
+	        }
+	    }	
+	    updateClock();
+	    var timeinterval = setInterval(updateClock, 1000);
+
+	    //on clik of next button 
+		$("#next").on("click", function(){
+			if(pointer !== 9){
+				$("#prev").removeClass("hidden");
+				if(document.getElementById('radio1').checked || document.getElementById('radio2').checked || document.getElementById('radio3').checked || document.getElementById('radio4').checked){
+					getSelected()
+					pointer++
+					populateQuestion(qBank,oBankA,oBankB,oBankC,oBankD,ansBank);
+				}
+				else{
+					alert("choose an option");
+				}
+			}
+			else{
+				clearInterval(timeinterval);
+				getSelected()
+				calcScore();
+				clap.play()
+			}
+		})
+	}
+
 	//Signout function
 	$("#btn-signout").on("click", function(){
 		firebase.auth().signOut().then(function() {
@@ -75,6 +154,7 @@ $(document).ready(function(){
 			ansBank.push(question[key].ans)	
 		}
 		populateQuestion(qBank, oBankA, oBankB,oBankC, oBankD, ansBank)
+		clockInit()
 		//popAnswer(qBank)
 	}
 
@@ -86,25 +166,6 @@ $(document).ready(function(){
 	document.getElementById("optionC").innerHTML = "C: " + " " + oBankC[pointer];
 	document.getElementById("optionD").innerHTML = "D: " + " " + oBankD[pointer];	
 	}
-
-	//on clik of next button 
-	$("#next").on("click", function(){
-		if(pointer !== 9){
-			if(document.getElementById('radio1').checked || document.getElementById('radio2').checked || document.getElementById('radio3').checked || document.getElementById('radio4').checked){
-				getSelected()
-				pointer++
-				populateQuestion(qBank,oBankA,oBankB,oBankC,oBankD,ansBank);
-			}
-			else{
-				alert("choose an option");
-			}
-		}
-		else{
-			getSelected()
-			calcScore();
-			clap.play()
-		}
-	})
 
 	//function to get getSelected
 	function getSelected(){
@@ -129,13 +190,6 @@ $(document).ready(function(){
 	      playerAnsBank.pop();
 	    }
 	})
-
-	
-	/*
-	$("#btn-quiz").on("click", function(){
-		window.location.href = "/?topic=" + subject;
-	})
-	*/
 
 	//function to calculate Score
 	function calcScore(){
@@ -164,17 +218,19 @@ $(document).ready(function(){
 
 	//function to display score
 	function displayScore(score){
-		hideThings();
+		hideQCS();
 		var par = "%"
 	    $(".mark").html("You scored: " + score + par);
 	}
 
 
-	function hideThings(){
+	function hideQCS(){
 		$(".question").hide();
 		$("#clockdiv").hide()
 		$(".score").removeClass("hidden");
 	}
+
+
 
 })
 
